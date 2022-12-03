267d3b0e19ca3a99a3101e890c5de4a4fab72436
joaopariz <parizoficial0102@outlook.com>
2022-12-03 15:35:59 -0300

    upload image form eventcontroller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -24,9 +24,25 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        
+
+        // Image upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName().strtotime("now")."." . $extension);
+
+            $requestImage->move(public_path('img/events'),$imageName);
+
+            $event->image = $imageName;
+
+        }
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso');
     }
 }
